4ebdd4626d81bb52487f01e2e152c3fe057a6ff0
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2021-04-23 09:43:27 +0200

    Add all repository classes and serivces

--- src/main/java/com/example/demo/Model/Product.java
+++ src/main/java/com/example/demo/Model/Product.java
@@ -1,14 +1,31 @@
 package com.example.demo.Model;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "Products")
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "name")
     private String name;
+
     private double price;
 
     public Product(long id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
+    }
+
+    public Product() {
+
     }
 
     public long getId() {
